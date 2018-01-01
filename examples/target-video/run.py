e2413e8bcb1cbcadd6d7039f91162d7cf87de33c
Roy Hyunjin Han <rhh@crosscompute.com>
2018-01-01 12:43:53 -0500

    Use different video

--- examples/target-video/run.py
+++ examples/target-video/run.py
@@ -5,6 +5,6 @@ from os.path import join
 
 if __name__ == '__main__':
     target_folder = sys.argv[1]
-    path = join(target_folder, 'bunny.mp4')
-    shutil.copy('bunny.mp4', path)
-    print('bunny_video_path = %s' % path)
+    path = join(target_folder, 'video.mp4')
+    shutil.copy('Puchica-20150620.mp4', path)
+    print('a_video_path = %s' % path)
